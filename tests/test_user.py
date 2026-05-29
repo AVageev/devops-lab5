c5135abae94ac2985de8c16ceb3d880378082783
Andrey <andrey.ageev.04@gmail.com>
2026-05-29 14:51:25 +0300

    add github actions

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,44 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'unknown@mail.com'})
+    
+    assert response.status_code == 404
+    assert response.json() == {
+        "detail": "User not found"
+    }
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {
+        "name": "Alex Smith",
+        "email": "alexsmith@mail.com"
+    }
+
+    response = client.post("/api/v1/user", json=new_user)
+
+    assert response.status_code == 201
+    assert type(response.json()) == int
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    existed_user = {
+        "name": "Ivan Ivanov",
+        "email": users[0]['email']
+    }
+
+    response = client.post("/api/v1/user", json=existed_user)
+
+    assert response.status_code == 409
+    assert response.json() == {
+        "detail": "User with this email already exists"
+    }
 
 def test_delete_user():
     '''Удаление пользователя'''
-    pass
+    response = client.delete(
+        "/api/v1/user",
+        params={"email": users[1]['email']}
+    )
+
+    assert response.status_code == 204
